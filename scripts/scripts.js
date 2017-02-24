5735fd4bc7ab0dd8164747af428faa097984c6b4
adamakers <adamrakers@gmail.com>
2017-02-24 16:42:59 -0700

    displaying cities in li

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,6 +2,7 @@
 const citiesURL = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 const formInpOne = document.querySelector('.input-one');
 const formInpTwo = document.querySelector('.input-two');
+const resultsList = document.querySelector('.results-list');
 
 let http = new XMLHttpRequest();
 let cities;
@@ -18,15 +19,24 @@ http.onreadystatechange = function() {
 }
 
 
-function filterList(theList) {
-  
+function filterCityList(searchFor, cities) {
+  const regex = new RegExp(searchFor, 'gi');
+  return cities.filter(function(place){
+    return place.city.match(regex) ||
+           place.state.match(regex);
+  });
 }
 
+function displayCities() {
+  const filteredList = filterCityList(this.value, cities);
+  const displayList = filteredList.map(function(item){
+    return `<li>${item.city}, ${item.state}</li>`;
+  });
+}
 
+// EVENT LISTENERS
 
-formInpOne.addEventListener("input", function(){
-  console.log(formInpOne.value);
-});
+formInpOne.addEventListener("input", displayCities);
 
 
 
